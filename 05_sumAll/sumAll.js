165050928da6a6138f108bfe0c379deaacfe3b95
Rade473 <r.shekularats@gmail.com>
2022-05-06 22:35:41 +0200

    Finished exercise my way

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
 
+if (a<0||b<0||isNaN(a)||isNaN(b)){
+    return x = 'ERROR';
+} 
+else if (a>b){
+    [a,b]=[b,a]
 };
+
+
+let range = Array.from({length: b}, (_x,i)=>i+a);
+let sum = ((a+b)*range.length)/2;
+return sum
+}
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
