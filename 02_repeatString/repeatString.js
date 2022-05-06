3a6272c8c26980b4a98755d1591fe469df705950
Rade473 <r.shekularats@gmail.com>
2022-05-06 17:17:39 +0200

    finished the exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word,n) {
+    let string = ''
+   if (n < 0 ) return 'ERROR'
+    for (j=0; j<n; j++){
+       string+=word
+    }
+    return string;
 };
 
 // Do not edit below this line
