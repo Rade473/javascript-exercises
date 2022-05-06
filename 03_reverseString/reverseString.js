9ca41a981cc812d217dfd3f8bbc57c4def71568d
Rade473 <r.shekularats@gmail.com>
2022-05-06 18:19:58 +0200

    Finished the exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
+function reverseString(string) {
+    string;
+    array = string.split('');
+    newarray = array.reverse();
+    newString = newarray.join('');
+    return newString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
