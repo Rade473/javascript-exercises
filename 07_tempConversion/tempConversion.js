dafe1fcb769c00b2d10c30770b849d711e85e525
Rade473 <r.shekularats@gmail.com>
2022-05-09 17:11:27 +0200

    Finished exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const ftoc = function() {
+const ftoc = function(F) {
+  C=(F-32)*5/9
+  return C = round (C,1);
+};
+
+const ctof = function(C) {
+  F=C*9/5+32;
+ return F = round (F,1);
 
 };
 
-const ctof = function() {
-
-};
-
+function round(value, precision) {
+  let multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 // Do not edit below this line
 module.exports = {
   ftoc,
