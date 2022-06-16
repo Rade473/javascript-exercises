31ba91ff32f8dd2bbba76566dfba1dcaae2a37ec
Rade473 <r.shekularats@gmail.com>
2022-06-16 18:40:48 +0200

    Finished exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
 const findTheOldest = function() {
-
+people.forEach(v => {v.age = people[yearOfDeath] - people [yearOfBirth]})
+//const peopleage = people.map(v => ({...v, age: people[v].yearOfDeath - people[v].yearOfBirth}))
+console.log(peopleage)
 };
 
 // Do not edit below this line
